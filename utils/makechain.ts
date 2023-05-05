337caac70226b88e21f62c80ae488b156663469e
SuuTTT <48884113+SuuTTT@users.noreply.github.com>
2023-05-05 14:10:35 +0000

    change port to 4000

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -11,25 +11,16 @@ Standalone question:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
 
 {context}
 
 Question: {question}
 Helpful answer in markdown:`;
-
-
-const CS_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
+//If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: 0.2, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
@@ -37,7 +28,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
     model,
     vectorstore.asRetriever(),
     {
-      qaTemplate: CS_PROMPT,
+      qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     },
